901a7da5d9fcd0a2f31effec29df73f2fc807b2d
Yujin Kim <as2132as@naver.com>
2022-09-25 14:11:45 +0900

    five commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express(); // express 서버 생성
 
 var port = 6608;
 
-
+// get : 읽기(사이트) , post : 쓰기(댓글작성), put: 수정, delete: 삭제(댓글) 
  // 메인 페이지
 app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + '/index.html');
@@ -15,6 +15,10 @@ app.listen(port,function(){
     console.log('listening ib 6608');
 }) // 서버 연결 
 
+app.get('/pet', function(요청, 응답){
+    응답.sendFile(__dirname + '/cat.html');
+    
+});
 //app.get('/pet', function(요청, 응답){
 //    응답.send('펫 용품 쇼핑 할 수 있는 페이지 입니다.');
     
