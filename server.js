3c4e5716ce26597eb4375e27756e6a1d1e590033
youjinee <as2132as@naver.com>
2022-10-25 12:54:44 +0900

    sever

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function(요청, 응답){
 });
 
 app.get('/search', function(요청, 응답){
-    응답.sendFile(__dirname + '/member.html');
+    응답.sendFile(__dirname + '/search.html');
     
 });
 app.get('/pet', function(요청, 응답){
