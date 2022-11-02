ffdfd13cb811034c277d74265fd8b16f3324119b
youjinee <as2132as@naver.com>
2022-11-02 19:54:00 +0900

    m

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express(); // express 서버 생성
 const port = process.env.PORT || 3000;
 const sqlite3 = require("sqlite3").verbose();
 
-//app.use(express.static(__dirname )); // nodejs에서는 css파일이 바로 안열림  서버에서 해당 파일을 제공하지 못함 따라서 express.static 미들웨어 함수에 해당 파일을 전달하면 됨
+app.use(express.static(__dirname )); // nodejs에서는 css파일이 바로 안열림  서버에서 해당 파일을 제공하지 못함 따라서 express.static 미들웨어 함수에 해당 파일을 전달하면 됨
 
 const db = new sqlite3.Database('C:user\OneDrive\바탕 화면\capstone\data\mapdata.db', sqlite3.OPEN_READWRITE, (err) => {
 
@@ -31,22 +31,22 @@ app.get('/search', function (요청, 응답) {
     응답.sendFile(__dirname + '/search.html');
 
     var hpname = `SELECT 병원명 from '병원';`;
-    db.all(hpname, function (err, rows) {
-        rows.forEach(function (row) {
-            console.log(row);
+    // db.all(hpname, function (err, rows) {
+    //     rows.forEach(function (row) {
+    //         console.log(row);
 
-            응답.render('hpname', {
-                hpname: row.병원명,
-                hpaddress: row.주소,
-                hptel: row.전화번호,
-                hppy: row.평일운영시간,
-                hpty: row.토요일운영시간,
-                hpiy: row.일요일운영시간
-            });
+    //         응답.render('hpname', {
+    //             hpname: row.병원명,
+    //             hpaddress: row.주소,
+    //             hptel: row.전화번호,
+    //             hppy: row.평일운영시간,
+    //             hpty: row.토요일운영시간,
+    //             hpiy: row.일요일운영시간
+    //         });
 
 
-        });
-    });
+    //     });
+    // });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
