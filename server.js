d0223e8aff2073e043acf778d522be8e7d218eca
youjinee <as2132as@naver.com>
2022-11-02 19:55:08 +0900

    오류 수정중

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const sqlite3 = require("sqlite3").verbose();
 
 app.use(express.static(__dirname )); // nodejs에서는 css파일이 바로 안열림  서버에서 해당 파일을 제공하지 못함 따라서 express.static 미들웨어 함수에 해당 파일을 전달하면 됨
 
-const db = new sqlite3.Database('C:user\OneDrive\바탕 화면\capstone\data\mapdata.db', sqlite3.OPEN_READWRITE, (err) => {
+// const db = new sqlite3.Database('C:user\OneDrive\바탕 화면\capstone\data\mapdata.db', sqlite3.OPEN_READWRITE, (err) => {
 
-    if (err) return console.error(err.message);
+//     if (err) return console.error(err.message);
 
-    console.log("connection successful");
+//     console.log("connection successful");
 
-});
+// });
 
 app.set('view engine', 'ejs');
 
@@ -30,7 +30,7 @@ app.get('/hi', function (요청, 응답) {
 app.get('/search', function (요청, 응답) {
     응답.sendFile(__dirname + '/search.html');
 
-    var hpname = `SELECT 병원명 from '병원';`;
+    //var hpname = `SELECT 병원명 from '병원';`;
     // db.all(hpname, function (err, rows) {
     //     rows.forEach(function (row) {
     //         console.log(row);
