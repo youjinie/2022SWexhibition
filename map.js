58fcec6e535c695da4bef8e4164a9846b64e2cf4
youjinee <as2132as@naver.com>
2022-11-08 16:50:16 +0900

    서버

--- map.js
+++ map.js
@@ -1,153 +1,38 @@
+
 $(document).ready(async function () {
     let XY = await getLocation(); // 사용자의 위도와 경도를 받아옴
-    //alert("위도 : " + XY.lat); // 위도 알림창
-    //alert("경도 : " + XY.lng); // 경도 알림창
+
     var map = new naver.maps.Map('map', { // 지도 띄우기
-        center: new naver.maps.LatLng(37.3595316, 127.1052133),
-        zoom: 7,
+        center: new naver.maps.LatLng(XY.lat, XY.lng),
+        zoom: 8,
         zoomControl: true, //줌 컨트롤의 표시 여부
         zoomControlOptions: { //줌 컨트롤의 옵션
             position: naver.maps.Position.TOP_RIGHT
         }
     });
+
+    maker(XY.lat, XY.lng); // 현재 위치에 마커 찍기 
+
+    function maker(lat, lng) { // 마커 찍는 함수 
+        var marker = new naver.maps.Marker({
+            position: new naver.maps.LatLng(lat, lng),
+            map: map
+        });
+    }
+    
+
     var infoWindow = new naver.maps.InfoWindow({
         anchorSkew: true
     });
 
-    // map.setCursor('pointer');
-
-    // function searchCoordinateToAddress(latlng) {
-
-    //     infoWindow.close();
-
-    //     naver.maps.Service.reverseGeocode({
-    //         coords: latlng,
-    //         orders: [
-    //             naver.maps.Service.OrderType.ADDR,
-    //             naver.maps.Service.OrderType.ROAD_ADDR
-    //         ].join(',')
-    //     }, function (status, response) {
-    //         if (status === naver.maps.Service.Status.ERROR) {
-    //             if (!latlng) {
-    //                 return alert('ReverseGeocode Error, Please check latlng');
-    //             }
-    //             if (latlng.toString) {
-    //                 return alert('ReverseGeocode Error, latlng:' + latlng.toString());
-    //             }
-    //             if (latlng.x && latlng.y) {
-    //                 return alert('ReverseGeocode Error, x:' + latlng.x + ', y:' + latlng.y);
-    //             }
-    //             return alert('ReverseGeocode Error, Please check latlng');
-    //         }
-
-    //         var address = response.v2.address,
-    //             htmlAddresses = [];
-
-    //         if (address.jibunAddress !== '') {
-    //             htmlAddresses.push('[지번 주소] ' + address.jibunAddress);
-    //         }
-
-    //         if (address.roadAddress !== '') {
-    //             htmlAddresses.push('[도로명 주소] ' + address.roadAddress);
-    //         }
-
-    //         infoWindow.setContent([
-    //             '<div style="padding:10px;min-width:200px;line-height:150%;">',
-    //             '<h4 style="margin-top:5px;">검색 좌표</h4><br />',
-    //             htmlAddresses.join('<br />'),
-    //             '</div>'
-    //         ].join('\n'));
-
-    //         infoWindow.open(map, latlng);
-    //     });
-    // }
-
-    // function searchAddressToCoordinate(address) {
-
-    //     naver.maps.Service.geocode({
-    //         query: address
-    //     }, function (status, response) {
-    //         if (status === naver.maps.Service.Status.ERROR) {
-    //             if (!address) {
-    //                 return alert('Geocode Error, Please check address');
-    //             }
-    //             return alert('Geocode Error, address:' + address);
-    //         }
-
-    //         if (response.v2.meta.totalCount === 0) {
-    //             return alert('No result.');
-    //         }
-
-    //         var htmlAddresses = [],
-    //             item = response.v2.addresses[0],
-    //             point = new naver.maps.Point(item.x, item.y);
-
-
-    //         if (item.roadAddress) {
-    //             htmlAddresses.push('[도로명 주소] ' + item.roadAddress);
-    //         }
-
-    //         if (item.jibunAddress) {
-    //             htmlAddresses.push('[지번 주소] ' + item.jibunAddress);
-    //         }
-
-    //         if (item.englishAddress) {
-    //             htmlAddresses.push('[영문명 주소] ' + item.englishAddress);
-    //         }
-
-    //         infoWindow.setContent([
-    //             '<div style="padding:10px;min-width:200px;line-height:150%;">',
-    //             '<h4 style="margin-top:5px;">검색 주소 : ' + address + '</h4><br />',
-    //             htmlAddresses.join('<br />'),
-    //             '</div>'
-    //         ].join('\n'));
-
-    //         map.setCenter(point);
-    //         infoWindow.open(map, point);
-    //     });
-    // }
-
-    // function initGeocoder() {
-    //     if (!map.isStyleMapReady) {
-    //         return;
-    //     }
-
-    //     map.addListener('click', function (e) {
-    //         searchCoordinateToAddress(e.coord);
-    //     });
-
-    //     $('#address').on('keydown', function (e) {
-    //         var keyCode = e.which;
-
-    //         if (keyCode === 13) { // Enter Key
-    //             searchAddressToCoordinate($('#address').val());
-    //         }
-    //     });
-
-    //     // $('#submit').on('click', function (e) {
-    //     //     e.preventDefault();
-
-    //     //     searchAddressToCoordinate($('#address').val());
-    //     // });
-
-    //     searchAddressToCoordinate('정자동 178-1');
-    // }
-
-    // naver.maps.onJSContentLoaded = initGeocoder;
-    // naver.maps.Event.once(map, 'init_stylemap', initGeocoder);
 
     var location = new naver.maps.LatLng(XY.lat, XY.lng); // 현재 위치 위도 경도
-
-    b1.onclick = function () {
-        var marker = new naver.maps.Marker({ // 마커찍기
-            position: new naver.maps.LatLng(XY.lat, XY.lng),
-            map: map
-        });
+    b1.onclick = function () { // 현재 위치 버튼 누를시 
         map.setCenter(location); // 얻은 좌표를 지도의 중심으로 설정합니다.
-        map.setZoom(17); // 지도의 줌 레벨을 변경합니다.
+        map.setZoom(17);
     };
-});
 
+});
 async function getLocation() { // 접속한사람 위치 조회
     let XY = new Object(); // 위치 반환할 것
 
@@ -160,10 +45,9 @@ async function getLocation() { // 접속한사람 위치 조회
         });
 
         let position = await promise;
-        //position.coords.latitude 위도
-        //position.coords.longitude 경도
-        XY.lat = position.coords.latitude;
-        XY.lng = position.coords.longitude;
+        XY.lat = position.coords.latitude; // 위도
+        XY.lng = position.coords.longitude; // 경도
     }
     return XY;
 }
+
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express(); // express 서버 생성
 const port = process.env.PORT || 3000;
 const sqlite3 = require("sqlite3").verbose();
 
-app.use(express.static(__dirname )); // nodejs에서는 css파일이 바로 안열림  서버에서 해당 파일을 제공하지 못함 따라서 express.static 미들웨어 함수에 해당 파일을 전달하면 됨
+app.use(express.static(__dirname)); // nodejs에서는 css파일이 바로 안열림  서버에서 해당 파일을 제공하지 못함 따라서 express.static 미들웨어 함수에 해당 파일을 전달하면 됨
 
-const db = new sqlite3.Database('C:user\OneDrive\바탕 화면\capstone\data\mapdata.db', sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database('./data/mapdata.db', sqlite3.OPEN_READWRITE, (err) => {
 
     if (err) return console.error(err.message);
 
@@ -14,7 +14,7 @@ const db = new sqlite3.Database('C:user\OneDrive\바탕 화면\capstone\data\map
 });
 
 app.set('view engine', 'ejs');
-
+app.use(express.json())
 
 // get : 읽기(사이트) , post : 쓰기(댓글작성), put: 수정, delete: 삭제(댓글) 
 // 메인 페이지
@@ -24,29 +24,96 @@ app.get('/', function (요청, 응답) {
 
 app.get('/hi', function (요청, 응답) {
     응답.sendFile(__dirname + '/example.html');
-
 });
 
 app.get('/search', function (요청, 응답) {
-    응답.sendFile(__dirname + '/search.html');
+    //응답.sendFile(__dirname + '/search.html');
 
-    var hpname = `SELECT 병원명 from '병원';`;
-    db.all(hpname, function (err, rows) {
+    var sql = `SELECT * from '병원';`;
+    db.all(sql, function (err, rows) {
         rows.forEach(function (row) {
-            console.log(row);
-
-            응답.render('hpname', {
-                hpname: row.병원명,
-                hpaddress: row.주소,
-                hptel: row.전화번호,
-                hppy: row.평일운영시간,
-                hpty: row.토요일운영시간,
-                hpiy: row.일요일운영시간
-            });
-
-
+            //console.log(rows[0]);
+            //응답.render("hospital", {model: row});
         });
+        응답.render('search',{model: rows});
+
+        
     });
+});
+
+app.get('/api/hospitals', function (요청, 응답) {
+
+    var sql = `SELECT * from '병원';`;
+    db.all(sql, function (err, rows) {
+        rows.forEach(function (row) {
+            //console.log(rows[0]);
+
+            //응답.render("hospital", {model: row});
+        });
+        응답.render('hospital', { model: rows });
+
+    });
+    /*const test = {
+        title: "Test",
+        items: ["one", "two", "three"]
+      };
+      응답.render("hospital", {model: test});*/
+});
+
+app.get('/api/pharmacy', function (요청, 응답) {
+
+    var sql = `SELECT * from '약국', '24시 편의점';`;
+    db.all(sql, function (err, rows) {
+        rows.forEach(function (row) {
+            //console.log(rows[0]);
+
+            //응답.render("hospital", {model: row});
+        });
+        응답.render('pharmacy', { model: rows });
+
+    });
+});
+
+app.get('/api/sbjinryoso', function (요청, 응답) {
+
+    var sql = `SELECT * from '선별진료소';`;
+    db.all(sql, function (err, rows) {
+        rows.forEach(function (row) {
+            //console.log(rows[0]);
+
+            //응답.render("hospital", {model: row});
+        });
+        응답.render('sbjinryoso', { model: rows });
+
+    });
+});
+
+app.get("/pharmach_list", (req, res) => {
+
+    let api = async () => {
+        let response = null;
+        try {
+            //http://api.odcloud.kr/api/3033733/v1/uddi:835b1c55-fbaf-45ee-936e-83b92b3c2a8d?page=1&perPage=10&returnType=XML&serviceKey=bb2DyTkLtZ69N9NXxrrn9JDou2Rshqy2nscuvs%2BPCKEv7NapMVsqMSJlxsXYHIs46GNI82Jp7qYApFEMokq5Jw%3D%3D
+            response = await axios.get("http://api.odcloud.kr/api/3033733/v1/uddi:835b1c55-fbaf-45ee-936e-83b92b3c2a8d", {
+                params: {
+                    "serviceKey": "bb2DyTkLtZ69N9NXxrrn9JDou2Rshqy2nscuvs+PCKEv7NapMVsqMSJlxsXYHIs46GNI82Jp7qYApFEMokq5Jw==",
+                    "page": req.query.page,
+                    "perPage": req.query.perPage,
+                    "returnType": req.query.returnType
+
+                }
+            });
+        }
+        catch (e) {
+            console.log(e);
+        }
+        return response;
+    }
+    api().then((response) => {
+        res.setHeader("Access Control Allow Origin", "*");
+        res.json(response.data.response.body);
+    });
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
